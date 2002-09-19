7a39fd33d500c6331573c6df561c292957973f5d
petric <petric>
2002-09-19 20:29:05 +0000

    additional error codes

---
+++ include/cvsfs_errno.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+                          cvsfs_errno.h  -  additional error numbers
+                             -------------------
+    begin                : Sat Sep 16 2002
+    copyright            : (C) 2002 by Petric Frank
+    email                : pfrank@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __CVSFS_ERRNO_H__
+#define __CVSFS_ERRNO_H__
+
+
+
+#define CVSFS_ENOTVERSIONED	1024	/* not a versioned item */
+#define	CVSFS_ENOTCHECKEDOUT	1025	/* item not checked out */
+
+
+
+#endif
